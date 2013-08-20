71c7b88a4d39a8c0d4752cd2a0c00a4e62437e83
Dickon Reed <dickon@cantab.net>
2013-08-20 20:17:20 +0100

    green bar: add another case

--- irc_test.go
+++ irc_test.go
@@ -11,6 +11,8 @@ func TestTokenizeString(t *testing.T) {
 	Is(result1, "")
 	Is(len(tokens1), 1)
 	Is(tokens1[0], "hello")
-
 	
+	tokens2, result2 := tokenizeString("hello:world")
+	Is(result2, "world")
+	Is(tokens2, []string{"hello"})
 }
