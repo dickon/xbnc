0d280d48d6de15ec4b79beb9e4ca3f6acaedf745
Dickon Reed <dickon@cantab.net>
2013-08-20 20:32:03 +0100

    test parsing a privmsg

--- irc_test.go
+++ irc_test.go
@@ -17,3 +17,12 @@ func TestTokenizeString(t *testing.T) {
 	Is(result2, "world")
 	Is(tokens2, []string{"hello"})
 }
+
+
+func TestParsePrivateMessage(t *testing.T) {
+	Terst(t)
+	
+	m := ParseMessage("PRIVMSG Vultan :Gordon's alive")
+	Compare(m.time, ">=", 1e9)
+	Is(m.command, "PRIVMSG")
+}
