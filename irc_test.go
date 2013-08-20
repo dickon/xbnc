43a7b5fb5633735dacca13d4e2b13f6c55dc15c4
Dickon Reed <dickon@cantab.net>
2013-08-20 20:15:27 +0100

    refactor: use meaningful names

--- irc_test.go
+++ irc_test.go
@@ -6,9 +6,11 @@ import (
 
 func TestTokenizeString(t *testing.T) {
 	Terst(t)
-	foo, bar := tokenizeString("hello")
+	tokens1, result1 := tokenizeString("hello")
 	
-	Is(bar, "")
-	Is(len(foo), 1)
-	Is(foo[0], "hello")
+	Is(result1, "")
+	Is(len(tokens1), 1)
+	Is(tokens1[0], "hello")
+
+	
 }
