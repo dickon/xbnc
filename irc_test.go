824889e215c583a7ec66a265d6f8d4b5629fcde1
Dickon Reed <dickon@cantab.net>
2013-08-20 20:19:45 +0100

    run go fmt

--- irc_test.go
+++ irc_test.go
@@ -1,17 +1,18 @@
 package main
+
 import (
-	"testing"
 	. "github.com/robertkrimen/terst"
+	"testing"
 )
 
 func TestTokenizeString(t *testing.T) {
 	Terst(t)
 	tokens1, result1 := tokenizeString("hello")
-	
+
 	Is(result1, "")
 	Is(len(tokens1), 1)
 	Is(tokens1[0], "hello")
-	
+
 	tokens2, result2 := tokenizeString("hello:world")
 	Is(result2, "world")
 	Is(tokens2, []string{"hello"})
