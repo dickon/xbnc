da5fef1ddf8976f322ca496a39c5637dfaa99d5b
Dickon Reed <dickon@cantab.net>
2013-08-26 15:53:15 +0100

    ensure conf.Login is populated

--- config.go
+++ config.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/user"
 )
 
 type Config struct {
@@ -37,12 +38,20 @@ func readConfig() {
 	}
 	if conf.Hostname == "" {
 		if conf.Hostname, err = os.Hostname(); err != nil {
-			fmt.Printf("Unable to determinte hostname: %v\n", err)
+			fmt.Printf("Unable to determine hostname: %v\n", err)
 			os.Exit(3)
 		}
 	}
 	if conf.Port == 0 {
 		conf.Port = 1234
+	}
+	if conf.Login == "" {
+		u, err := user.Current()
+		if err != nil {
+			fmt.Printf("Unable to determinte user name: %v\n", err)
+			os.Exit(4)
+		}
+		conf.Login = u.Username
 	}
 	for i, elem := range conf.Servers {
 		fmt.Printf("Server %d: %s:%d\n", i+1, elem.Host, elem.Port)
