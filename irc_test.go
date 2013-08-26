977e47373fbb9524fc92976b9552fadd19f1579f
Dickon Reed <dickon@cantab.net>
2013-08-26 14:38:21 +0100

    add more test code

--- irc_test.go
+++ irc_test.go
@@ -18,10 +18,15 @@ func TestTokenizeString(t *testing.T) {
 	Is(tokens2, []string{"hello"})
 }
 
-func TestParsePrivateMessage(t *testing.T) {
+func TestParseMessage(t *testing.T) {
 	Terst(t)
 
 	m := ParseMessage("PRIVMSG Vultan :Gordon's alive")
 	Compare(m.time, ">=", 1e9)
 	Is(m.command, "PRIVMSG")
+
+	m = ParseMessage(":irc.example.com 001 MyNickname :Welcome to the Internet Relay Network MyNickname!~MyUsername@client.example.com")
+	Compare(m.time, ">=", 1e9)
+	Is(m.command, "REPLY")
+
 }
